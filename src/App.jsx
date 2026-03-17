326d67fa5e6ce31cb769e1680bbdc60d7856da46
odon-airoldi <odon.airoldi@gmail.com>
2026-03-17 16:20:36 +0100

    add actors render

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import './App.css'
 function App() {
 
   const actressesApi = 'https://lanciweb.github.io/demo/api/actresses/'
+  const actorsApi = 'https://lanciweb.github.io/demo/api/actors/'
 
   const [actresses, setActresses] = useState([])
+  const [actors, setActors] = useState([])
 
-  function loadActresses() {
-    fetch(actressesApi)
+  function loadAct(urlApi, setFunction) {
+    fetch(urlApi)
       .then(res => res.json())
       .then(data => {
-        setActresses(data)
+        setFunction(data)
       })
   }
 
-  useEffect(() => { loadActresses() }, [])
+  useEffect(() => { loadAct(actressesApi, setActresses) }, [])
+  useEffect(() => { loadAct(actorsApi, setActors) }, [])
 
 
 
@@ -27,9 +30,37 @@ function App() {
           <div className="row g-2">
             {
               actresses.map(actress => (
-                <div className="col-4" key={actress.id}>
-                  <div className="card">
-                    <img className="card-img-top" src={actress.image} alt={actress.name} />
+                <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2" key={actress.id}>
+                  <div className="card h-100">
+                    <div className="ratio ratio-1x1">
+                      <img className="card-img-top object-fit-cover" src={actress.image} alt={actress.name} />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">{actress.name}</h5>
+                      <ul className="small list-unstyled">
+                        <li>{actress.birth_year}</li>
+                        <li>{actress.nationality}</li>
+                        {
+                          actress.awards.map((award, i) => (
+                            <li key={i}>{award}</li>
+                          ))
+                        }
+                      </ul>
+                      <p className="small">{actress.biography}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+          <div className="row g-2">
+            {
+              actors.map(actress => (
+                <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2" key={actress.id}>
+                  <div className="card h-100">
+                    <div className="ratio ratio-1x1">
+                      <img className="card-img-top object-fit-cover" src={actress.image} alt={actress.name} />
+                    </div>
                     <div className="card-body">
                       <h5 className="card-title">{actress.name}</h5>
                       <ul className="small list-unstyled">
